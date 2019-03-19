796053ddc574a1fcf5129240be02c2da20513e19
Deathgun998 <davi.fuma@hotmail.com>
2019-03-19 12:59:14 +0100

    creazione classe login con layout aggiunta dei 3 loghi

---
+++ app/src/main/java/com/example/androidadvanced201819/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidadvanced201819;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//       EditText usernameEditText = (EditText) findViewById(R.id.editTextUser);
+//       EditText password = (EditText) findViewById(R.id.editTextPasw);
+//       Button loginButton = (Button) findViewById(R.id.loginButton);
+
+
+    }
+
+}
