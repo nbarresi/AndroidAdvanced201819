62e128249782ef3a5fdd590e14199b46c030acf0
giacomo.mangugan <giacomomangugan98@gmail.com>
2019-03-27 12:29:23 +0100

    added Event OnLongPress to Adapter
    To delete Profile in MainActivity

--- app/src/main/java/com/example/androidadvanced201819/activities/profile/MainActivity.java
+++ app/src/main/java/com/example/androidadvanced201819/activities/profile/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.androidadvanced201819.activities.profile;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.androidadvanced201819.activities.AdapterActivity;
 import com.example.androidadvanced201819.dataaccess.DataAccessUtils;
 import com.example.androidadvanced201819.R;
+import com.example.androidadvanced201819.database.ProfileDatabaseManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +28,47 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
 
         listView.setAdapter(adapterActivity);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> av, View v, final int pos, long id)
+            {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                // set title
+                alertDialogBuilder.setTitle("Delete profile?");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                ProfileDatabaseManager profileDatabaseManager;
+                                // if this button is clicked, close
+                                // current activity
+                                profileDatabaseManager = new ProfileDatabaseManager(MainActivity.this);
+                                profileDatabaseManager.open();
+                                profileDatabaseManager.deleteProfile(DataAccessUtils.getItemByPosition(MainActivity.this, pos).getNome());
+                                profileDatabaseManager.close();
+                                DataAccessUtils.removeItem(DataAccessUtils.getItemByPosition(MainActivity.this, pos), MainActivity.this);
+                                adapterActivity.setValues();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close
+                                // the dialog box and do nothing
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+                return true;
+            }
+        });
         setTitle("Lista profili");
     }
 
