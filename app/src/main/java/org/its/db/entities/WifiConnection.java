4186753a80adf20fc2dcafc9ce9c4b7e283301c0
matteo-bellinaso <matteo.bellinaso@gmail.com>
2019-03-28 16:51:40 +0100

    WifiConnection

---
+++ app/src/main/java/org/its/db/entities/WifiConnection.java
@@ -0,0 +1,60 @@
+package org.its.db.entities;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class WifiConnection implements Serializable {
+
+    private int power;
+    private String name;
+    private String BSSID;
+
+    public WifiConnection() {
+    }
+
+    public WifiConnection(int power, String name, String BSSID) {
+        this.power = power;
+        this.name = name;
+        this.BSSID = BSSID;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBSSID() {
+        return BSSID;
+    }
+
+    public void setBSSID(String BSSID) {
+        this.BSSID = BSSID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WifiConnection that = (WifiConnection) o;
+        return power == that.power &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(BSSID, that.BSSID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(power, name, BSSID);
+    }
+}
