cd900737c7b352e3a5cf1c28bc025368efef50c9
matteo-bellinaso <matteo.bellinaso@gmail.com>
2019-04-03 09:31:02 +0200

    adapter

---
+++ app/src/main/java/org/its/UI/WifiArrayAdapter.java
@@ -0,0 +1,46 @@
+package org.its.UI;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.androidadvanced201819.R;
+
+import java.util.List;
+
+public class WifiArrayAdapter extends ArrayAdapter<ScanResult> {
+
+    private List<ScanResult> list;
+    private Context context;
+
+    public WifiArrayAdapter( Context context, List<ScanResult> values) {
+        super(context, R.layout.wifi_list_item_layout ,values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.wifi_list_item_layout, parent, false); //converte il layout da XML a oggetto view
+
+        TextView wifiPower = (TextView) rowView.findViewById(R.id.wifiPower);
+        TextView wifiBssid = (TextView) rowView.findViewById(R.id.wifiBSSID);
+        TextView wifiName = (TextView) rowView.findViewById(R.id.wifiName);
+        ScanResult itemName = this.list.get(position);
+        wifiPower.setText(String.valueOf(itemName.level) + "%");
+        wifiBssid.setText(String.valueOf(itemName.BSSID));
+        wifiName.setText(String.valueOf(itemName.SSID));
+
+        return rowView;
+    }
+
+    @Override
+    public int getCount(){
+        return this.list.size();
+    }
+
+}
